73171ae1a7953aba7904e0ca6eb4e1f2d944944f
awes0m <somsubhra.pandit@gmail.com>
2021-08-21 21:38:24 +0530

    Dummy data displayed in homescreen

--- tour_apisod/lib/categories_screen.dart
+++ tour_apisod/lib/categories_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:tour_apisod/category_item.dart';
+import 'package:tour_apisod/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GodlyMeals"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catdata) => CategoryItem(
+                catdata.title,
+                catdata.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        padding: EdgeInsets.all(20),
       ),
     );
   }

c580b8644b265a925b59dced88e18551f6ece7c9
awes0m <somsubhra.pandit@gmail.com>
2021-08-22 14:20:06 +0530

    theme changes

--- tour_apisod/lib/main.dart
+++ tour_apisod/lib/main.dart
@@ -12,7 +12,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Godlymeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        // canvasColor: Color.alphaBlend(
+        //   Colors.white,
+        //   Colors.amber,
+        // ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            headline6: TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed')),
       ),
       home: CategoriesScreen(),
     );
